4ece644dc89854a2fe2d2513c80a6f88d5400fb1
Huggepugge1 <Huggepugge1@gmail.com>
2023-12-10 19:48:30 +0100

    Final

--- index.js
+++ index.js
@@ -12,22 +12,38 @@ const changeWishlist = (element) => {
             .forEach((element) => {
                 const div = document.createElement("div");
                 div.className = "wish";
+                const front = document.createElement("div");
+                front.className = "wish-front";
+                div.appendChild(front);
                 const cover = document.createElement("div");
-                cover.className = "cover";
+                cover.className = "wish-cover";
                 div.appendChild(cover);
+                const back = document.createElement("div");
+                back.className = "wish-back";
 
                 const header = document.createElement("h3");
                 header.appendChild(document.createTextNode(element[0]))
                 cover.appendChild(header);
-
+                
+                if (element[1].description !== undefined) {
+                    const desc = document.createElement("p");
+                    desc.innerHTML = element[1].description;
+                    back.appendChild(desc);
+                }
+                
                 if (element[1].link !== undefined) {
                     div.addEventListener("click", () => window.location.href = element[1].link);
+                    const click_me = document.createElement("p");
+                    click_me.innerHTML = "(Klicka för att köpa)";
+                    back.appendChild(click_me);
                 }
 
                 if (element[1].image !== undefined) {
-                    div.style.backgroundImage = `url(${element[1].image})`;
+                    front.style.backgroundImage = `url(${element[1].image})`;
                 }
 
+
+                div.appendChild(back);
                 wishlist.appendChild(div);
             })
     });
